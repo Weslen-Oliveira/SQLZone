3da1e43c1a4dae333449080650970ec53fbfff34
Weslen-Oliveira <weslenoliveira710@gmail.com>
2025-05-28 19:12:57 -0300

    tabela nv1

--- nivel1.js
+++ nivel1.js
@@ -1,4 +1,6 @@
 const TituloNv1 = document.getElementById('tituloNv1');
+let tituloQuestao = document.getElementById('tituloQuestao');
+let paragrafoQuestao = document.getElementById('paragrafoQuestao');
 const Comando = document.getElementById('comando');
 const tabelaOK = document.getElementById('tabelaOK');
 const tabelaNOK = document.getElementById('tabelaNOK');
@@ -41,6 +43,19 @@ Enviar.addEventListener('click', function Responder(){
 proximo.addEventListener('click', function proximaPergunta(){
     tabelaOK.style.display = 'none';
     tabelaNOK.style.display = 'none';
-
+    tituloQuestao.innerText = 'Vamos manipular a tabela Familia';
+    paragrafoQuestao.innerText = 'Insira dados na tabela Familia';
     
+
+    let id = document.getElementById('tabelaId').value;
+    let nome = document.getElementById('tabelaNome').value;
+    let categoria = document.getElementById('tabelaCategoria').value;
+    let sexo = document.getElementById('tabelaSexo').value;
+    let idade = document.getElementById('tabelaIdade').value;
+
+    if(verificarResposta === 'INSERT INTO Familia (id, nome, categoria, sexo, idade) VALUES'){
+        tabela02.style.display = 'block';
+         proximo.style.display = 'inline-block';
+        tabelaNOK.style.display = 'none'
+    }
 })
